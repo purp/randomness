ba30a3227ff57798c2460187202599dc9c152955
Jim Meyer <jim@geekdaily.org>
2014-12-20 14:25:31 -0800

    Add rbenv and ruby-build

--- cloud-init.sh
+++ cloud-init.sh
@@ -7,6 +7,27 @@ yum update -y
 yum groupinstall "Development Tools"
 yum install bind-utils
 
+# Install rbenv and ruby-build for all users a la http://blakewilliams.me/blog/system-wide-rbenv-install
+cd /usr/local
+git clone git://github.com/sstephenson/rbenv.git rbenv
+chgrp -R staff rbenv
+chmod -R g+rwxXs rbenv
+
+cat <<RBENV_SH
+export RBENV_ROOT=/usr/local/rbenv
+export PATH="$RBENV_ROOT/bin:$PATH"
+eval "$(rbenv init -)"
+RBENV_SH > /etc/profile.d/rbenv.sh
+
+cd /usr/local/rbenv
+mkdir plugins
+cd plugins
+git clone git://github.com/sstephenson/ruby-build.git
+chgrp -R staff ruby-build
+chmod -R g+rwxs ruby-build
+
+
+
 # Set up non-root user
 adduser purp --groups wheel --password '$1$JGqmDake$B8nv8GHvD5sPw92txYIe4/'
 chage -d 0 purp
