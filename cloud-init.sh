1f06331468191206716cb4a74956e475bef3cc18
Jim Meyer <jim@geekdaily.org>
2014-12-20 13:15:07 -0800

    Create cloud-init.sh

---
+++ cloud-init.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# disable ssh passwords and restart sshd
+sed -i -e 's/^PasswordAuthentication yes/PasswordAuthentication no/' /etc/ssh/sshd_config && service sshd restart
+# update system packages & install some things
+yum update -y
+yum install bind-utils
+
+# Set up non-root user
+adduser purp --groups wheel --password '$1$JGqmDake$B8nv8GHvD5sPw92txYIe4/'
+chage -d 0 purp
+mkdir ~purp/.ssh && cp -p ~/.ssh/authorized_keys ~purp/.ssh/. && chown -R purp:purp ~purp/.ssh && chmod -R go-rwx ~purp/.ssh
